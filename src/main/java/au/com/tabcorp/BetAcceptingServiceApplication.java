4f42b6909734a8aec81ff5d4391c0a0a79d38ac2
Jiang Liu <liujiang682@gmail.com>
2019-04-25 14:35:59 +1000

    first commit

---
+++ src/main/java/au/com/tabcorp/BetAcceptingServiceApplication.java
@@ -0,0 +1,13 @@
+package au.com.tabcorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetAcceptingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetAcceptingServiceApplication.class, args);
+	}
+
+}
